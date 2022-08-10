fe245e802c48979249e2a81abb1cdea7ea66b4cd
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-10 11:25:28 +0200

    finish

--- Piedra-papel-tijera/app.js
+++ Piedra-papel-tijera/app.js
@@ -1,41 +1,51 @@
-//1 es piedra, 2 es papel, 3 es tijera 
-function aleatorio(min, max){
-    return Math.floor(Math.random() * (max - min +1) + min)
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function eleccion(jugada) {
     let resultado = ""
-    if(jugada == 1){
-        resultado = "Piedra 🪨 "
-    } else if(jugada == 2){
-        resultado = "Papel 📃 " 
-    } else if(jugada == 3){
-        resultado = "Tijera  ✂️ "
-    } else{
-        resultado = "Debes elegir una opcion"
+    if (jugada == 1) {
+        resultado = "Piedra 🪨"
+    } else if (jugada == 2) {
+        resultado = "Papel 📃"
+    } else if (jugada == 3) {
+        resultado = "Tijera ✂️"
+    } else {
+        resultado = "MAL ELEGIDO"
     }
     return resultado
 }
 
-
-
+// 1 es piedra, 2 es papel, 3 es tijera
 let jugador = 0
-let pc = aleatorio(1,3)
-jugador = prompt("Elige:1 para piedra, 2 para pepele, 3 para tijera")
-//alert("Elegistes " + jugador)
+let pc = 0
+let triunfos = 0
+let perdidas = 0
 
-alert("PC elige: " + eleccion(pc))
-alert("Tu eliges: " + eleccion(jugador))
+while (triunfos < 3 && perdidas < 3) {
+    pc = aleatorio(1, 3)
+    jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera")
+    // alert("Elegiste " + jugador)
 
-//COMBATE
-if(pc == jugador){
-    alert("Empate")
-}else if(jugador ==1 && pc == 3){
-    alert("Ganaste")
-}else if(jugador == 2 && pc== 1){
-    alert("Ganastes")}
-else if(jugador == 3 && pc== 1){
-    alert("Ganastes")
-} else {
-    alert("Perdiste")
+    alert("PC elige " + eleccion(pc))
+    alert("Tú eliges " + eleccion(jugador))
+
+    // COMBATE
+    if (pc == jugador) {
+        alert("EMPATE")
+    } else if (jugador == 1 && pc == 3) {
+        alert("GANASTE")
+        triunfos = triunfos + 1
+    } else if (jugador == 2 && pc == 1) {
+        alert("GANASTE")
+        triunfos = triunfos + 1
+    } else if (jugador == 3 && pc == 2) {
+        alert("GANASTE")
+        triunfos = triunfos + 1
+    } else {
+        alert("PERDISTE")
+        perdidas = perdidas + 1
+    }
 }
+
+alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + " veces.")
