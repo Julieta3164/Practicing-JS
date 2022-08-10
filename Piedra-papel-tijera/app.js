c47ec2f0edcbfc7dfcfb3394854efda70c1ec24f
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-10 10:25:29 +0200

    funcion eleccion

--- Piedra-papel-tijera/app.js
+++ Piedra-papel-tijera/app.js
@@ -2,27 +2,31 @@
 function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min +1) + min)
 }
+
+function eleccion(jugada) {
+    let resultado = ""
+    if(jugada == 1){
+        resultado = "Piedra 🪨 "
+    } else if(jugada == 2){
+        resultado = "Papel 📃 " 
+    } else if(jugada == 3){
+        resultado = "Tijera  ✂️ "
+    } else{
+        resultado = "Debes elegir una opcion"
+    }
+    return resultado
+}
+
+
+
 let jugador = 0
 let pc = aleatorio(1,3)
 jugador = prompt("Elige:1 para piedra, 2 para pepele, 3 para tijera")
 //alert("Elegistes " + jugador)
-if(jugador == 1){
-    alert("Elegistes 🪨 " )
-} else if(jugador == 2){
-    alert("Elegistes 📃 ") 
-} else if(jugador == 3){
-    alert("Elegistes ✂️ ")
-} else{
-    alert("Elegistes  PERDER")
-}
 
-if(pc == 1){
-    alert("PC elige 🪨 " )
-} else if(pc == 2){
-    alert("PC elige📃 ") 
-} else if(pc == 3){
-    alert("PC elige ✂️ ")
-} 
+alert("PC elige: " + eleccion(pc))
+alert("Tu eliges: " + eleccion(jugador))
+
 //COMBATE
 if(pc == jugador){
     alert("Empate")
