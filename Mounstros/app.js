e480e967c03d813ef6f35806892733a88fdfd8ce
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-10 12:20:48 +0200

    funcion selecionar mascotas

---
+++ Mounstros/app.js
@@ -0,0 +1,7 @@
+function selecionarMascota(){
+    alert("selecionado")
+}
+
+
+let botonMascota = document.getElementById('boton-mascota')
+botonMascota.addEventListener('click', selecionarMascota)
