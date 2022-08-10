538ea243e97ce4fd9d48fb64ecbb0a4eda38427b
Juliet Linarez <julietlinarez@Mac-mini-de-Heber.local>
2022-08-10 09:16:25 +0200

    condicionales

--- Piedra-papel-tijera/app.js
+++ Piedra-papel-tijera/app.js
@@ -3,3 +3,13 @@
 let jugador = 0
 let pc = 2
 jugador = prompt("Elige:1 para piedra, 2 para pepele, 3 para tijera")
+//alert("Elegistes " + jugador)
+if(jugador == 1){
+    alert("Elegistes piedra" )
+} else if(jugador == 2){
+    alert("Elegistes papel")
+} else if(jugador == 3){
+    alert("Elegistes  tijera")
+} else{
+    alert("Elegistes  perder")
+}
